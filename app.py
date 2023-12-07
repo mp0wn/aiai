fe2ac1175a607437fb59ea796fef9c0d5a359d98
mp0wn <153237661+mp0wn@users.noreply.github.com>
2023-12-07 17:33:22 -0500

    1

---
+++ app.py
@@ -0,0 +1,8 @@
+import os 
+import streamlit as st
+from langchain.llms import OpenAI
+from apikey import apikey
+os.environ["OPEN_API_KEY"]=apikey
+st.title('Medium Article Generator')
+topic = st.text_input('Input your topic of interest')
+llm = OpenAI(temperature=0.9)
